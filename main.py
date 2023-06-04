f354f3b72e7248447189416e69cca757fd2cf619
Yegor2203 <134845627+Yegor2203@users.noreply.github.com>
2023-06-04 15:12:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import pygame
+from pygame import *
 from time import time as timer
 
 back = (200, 255, 255)
-win_widht = 600
+win_width = 700
 win_height = 500
-window = display.set_mode((win_height, win_widht))
+display.set_caption("Ping-Pong")
+window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 game = True
 clock = time.Clock()
-
+racket1 = Player()
 while game:
 
-    for event in event.get():
-        if e.type == pygame.QUIT:
+    for e in event.get():
+        if e.type == QUIT:
             game = False
     
     display.update
