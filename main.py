5b464c019eedee1a775522bf625a7619decf0b75
Yegor2203 <134845627+Yegor2203@users.noreply.github.com>
2023-06-04 15:26:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        
+
 back = (200, 255, 255)
 win_width = 700
 win_height = 500
@@ -20,14 +34,21 @@ display.set_caption("Ping-Pong")
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+racket1 = Player("racket.png", 30, 200, 4, 50, 150)
+racket2 = Player("racket.png", 520, 200, 4, 50, 150)
+ball = GameSprite("tenis_ball.png", 200, 200, 4, 50, 50)
 game = True
 clock = time.Clock()
-racket1 = Player()
+
 while game:
 
     for e in event.get():
         if e.type == QUIT:
             game = False
     
+
+    racket1.reset()
+    racket2.reset()
+
     display.update
     clock.tick(60)
