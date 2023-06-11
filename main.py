8b7b1db8a32887043d76362e107d6c80f7f03710
Yegor2203 <134845627+Yegor2203@users.noreply.github.com>
2023-06-11 15:13:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pygame import *
-from time import time as timer
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = image.load(player_image)
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -14,41 +13,76 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
+        keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
     def update_l(self):
-        if keys[K_W] and self.rect.y > 5:
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_S] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
         
 
 back = (200, 255, 255)
-win_width = 700
+win_width = 600
 win_height = 500
 display.set_caption("Ping-Pong")
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
-racket1 = Player("racket.png", 30, 200, 4, 50, 150)
-racket2 = Player("racket.png", 520, 200, 4, 50, 150)
-ball = GameSprite("tenis_ball.png", 200, 200, 4, 50, 50)
+racket1 = Player("racket.png", 30, 200, 1, 50, 150)
+racket2 = Player("racket.png", 520, 200, 1, 50, 150)
+ball = GameSprite("tenis_ball.png", 200, 200, 1, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
+finish = False
 game = True
 clock = time.Clock()
+
+font.init()
+Myfont = font.Font(None, 36)
+lose1 = Myfont.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = Myfont.render('PLAYER 2 LOE!', True, (180, 0, 0))
 
 while game:
 
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y    
+        
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        
+        if  ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game = True
+
+        if ball.rect.y > win_width:
+            window.blit(lose2, (200, 200))
+            game = True
     
-
-    racket1.reset()
-    racket2.reset()
-
-    display.update
-    clock.tick(60)
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        
+        
+    display.update()
+    clock.tick(40)
